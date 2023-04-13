8ba942d91f29515dcb3dd8604e4b08d6fac117e7
LeiMeiLing <2443030912@qq.com>
2023-04-13 09:35:26 +0800

    添加了一个用户转让申请表和逆向工程，修改了转让信息表的字段

--- pet-adoption-system-api/src/main/java/cn/jasonone/bean/TransferApplication.java
+++ pet-adoption-system-api/src/main/java/cn/jasonone/bean/TransferApplication.java
@@ -5,18 +5,18 @@ import java.util.Date;
 import lombok.Data;
 
 /**
- * 转让信息表
- * @TableName transfer_info
+ * 转让申请信息表
+ * @TableName transfer_application
  */
 @Data
-public class TransferInfo implements Serializable {
+public class TransferApplication implements Serializable {
     /**
-     * 转让编号
+     * 转让id
      */
     private Integer id;
 
     /**
-     * 转让标题
+     * 领养用户名
      */
     private String title;
 
@@ -31,29 +31,9 @@ public class TransferInfo implements Serializable {
     private String transferName;
 
     /**
-     * 转让情况说明
+     * 申请说明
      */
     private String transferCause;
-
-    /**
-     * 宠物照片
-     */
-    private String picture;
-
-    /**
-     * 种类
-     */
-    private String variety;
-
-    /**
-     * 性别
-     */
-    private String petSex;
-
-    /**
-     * 城市
-     */
-    private String city;
 
     /**
      * 状态
