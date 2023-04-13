5441505748fca3f72423c689803ffb47e6aa5a70
董事长专用 <1909587084@qq.com>
2023-04-13 15:09:58 +0800

    管理员界面的商品管理界面，能进行添加（其他的没写）

---
+++ pet-adoption-system-web/src/views/manager/component/ShoppingManger/api.js
@@ -0,0 +1,59 @@
+import http from "../../../../config/http.js";
+
+/*
+    更新用户信息
+ */
+// export function updateUser(row) {
+//     return http.put("/user/update",{
+//         "id":row.id,
+//         "username":row.username,
+//         "email":row.email,
+//         "phone":row.phone
+//
+//     }).then(res=>{
+//         return res;
+//     })
+// }
+
+/*
+查询所有
+ */
+export function list(){
+    return http.get("/petstore/findAll",{
+    }).then(res=>{
+        return res;
+    })
+}
+// /*
+// 模糊查询
+//  */
+// export function findSome(username,email,phone){
+//     return http.get("/user/findSome?userName="+username+"&email="+email+"&phone="+phone,{
+//     }).then(res=>{
+//         return res;
+//     })
+// }
+// /*
+// 删除用户方法
+//  */
+// export function deleteUserInfo(row){
+//     return http.delete("/user", {
+//         data:{
+//             "id":row.id
+//         }
+//     },).then(res=>{
+//         return res;
+//     })
+// }
+
+export function add(petStoreAdd){
+    return http.put("/petstore/add",{
+        goodsType: petStoreAdd.goodsType,
+        goodsname: petStoreAdd.goodsname,
+        goodsPrice: petStoreAdd.goodsPrice,
+        goodsDesc: petStoreAdd.goodsDesc,
+        goodsStatus: petStoreAdd.goodsStatus,
+    }).then(res=>{
+        return res
+})
+}
