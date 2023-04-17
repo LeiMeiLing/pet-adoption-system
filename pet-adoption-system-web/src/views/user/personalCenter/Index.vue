b6affa41fef3f9ea395ece2239229465ded38836
Qinqianqian6 <3522285284@qq.com>
2023-04-17 19:51:26 +0800

    个人信息界面完善

--- pet-adoption-system-web/src/views/user/personalCenter/Index.vue
+++ pet-adoption-system-web/src/views/user/personalCenter/Index.vue
@@ -4,22 +4,20 @@
       <lay-side>
         <lay-menu class="menu" :selected-key="selectedKey" @change-selected-Key="changeSelectedKey"
                   @change-open-keys="changeOpenKeys" v-model:openKeys="openKeys2" :tree="true">
-          <lay-menu-item id="1" to="center">个人中心</lay-menu-item>
-          <lay-menu-item id="2" @click="visible1=true">个人信息</lay-menu-item>
-          <lay-menu-item id="3" @click="visible2=true">收货地址</lay-menu-item>
-          <lay-menu-item id="4" @click="visible3=true">修改密码</lay-menu-item>
-          <lay-menu-item id="5" to="/application">领养申请</lay-menu-item>
-          <lay-menu-item id="6">转送宠物</lay-menu-item>
-          <lay-menu-item id="8" to="myCart">我的购物车</lay-menu-item>
-          <lay-menu-item id="7" @click="exit">退出登录</lay-menu-item>
+          <lay-menu-item id="10" to="/home"><lay-icon type="layui-icon-home"></lay-icon>&emsp;首&emsp;&emsp;页</lay-menu-item>
+          <lay-menu-item id="1" to="center"><lay-icon type="layui-icon-username"></lay-icon>&emsp;个人中心</lay-menu-item>
+          <lay-menu-item id="2" to="/information"><lay-icon type="layui-icon-edit"></lay-icon>&emsp;个人信息</lay-menu-item>
+          <lay-menu-item id="3" @click="visible2=true"><lay-icon type="layui-icon-location"></lay-icon>&emsp;收货地址</lay-menu-item>
+          <lay-menu-item id="4" @click="visible3=true"><lay-icon type="layui-icon-key"></lay-icon>&emsp;修改密码</lay-menu-item>
+          <lay-menu-item id="5" to="/application"><lay-icon type="layui-icon-log"></lay-icon>&emsp;领养申请</lay-menu-item>
+          <lay-menu-item id="6"><lay-icon type="layui-icon-time"></lay-icon>&emsp;转送宠物</lay-menu-item>
+          <lay-menu-item id="8" to="myCart"><lay-icon type="layui-icon-cart-simple"></lay-icon>&emsp;购物车</lay-menu-item>
+          <lay-menu-item id="9" @click="exit"><lay-icon type="layui-icon-logout"></lay-icon>&emsp;退出登录</lay-menu-item>
         </lay-menu>
       </lay-side>
       <lay-body>
         <router-view></router-view>
       </lay-body>
-      <lay-layer :title="'个人信息'" v-model="visible1">
-        <information v-model="visible1"></information>
-      </lay-layer>
       <lay-layer :title="'收货地址'" v-model="visible2">
         <Address/>
       </lay-layer>
--- pet-adoption-system-web/src/views/user/personalCenter/information/Information.vue
+++ pet-adoption-system-web/src/views/user/personalCenter/information/Information.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="information">
-    <lay-form v-model="visible">
-      <lay-avatar :src="src" radius></lay-avatar>
-      <lay-form-item label="年龄：">
-        <lay-input v-model="userInfo.age"></lay-input>
-      </lay-form-item>
-      <lay-form-item label="性别：">
-        <lay-select v-model="value" placeholder="请选择" @change="change1">
-          <lay-select-option :value="1" label="男"></lay-select-option>
-          <lay-select-option :value="2" label="女"></lay-select-option>
-        </lay-select>
-
-      </lay-form-item>
-      <lay-form-item label="手机号：">
-        <lay-input v-model="userInfo.phone"></lay-input>
-      </lay-form-item>
-      <lay-form-item label="邮箱地址：">
-        <lay-input v-model="userInfo.email"></lay-input>
-      </lay-form-item>
-      <lay-form-item label="个性签名："></lay-form-item>
-      <lay-textarea placeholder="请输入描述" v-model="userInfo.sign"></lay-textarea>
-      <br>
-      <lay-button type="primary" fluid @click="onUpdate()">保存个人信息</lay-button>
-    </lay-form>
-  </div>
+    <lay-panel class="information">
+      <lay-form v-model="visible">
+        <h1>个人信息</h1>
+        <lay-avatar :src="src" radius></lay-avatar>
+        <lay-form-item label="年龄：">
+          <lay-input v-model="userInfo.age"></lay-input>
+        </lay-form-item>
+        <lay-form-item label="性别：">
+          <lay-select v-model="value" placeholder="请选择" @change="change1">
+            <lay-select-option :value="1" label="男"></lay-select-option>
+            <lay-select-option :value="2" label="女"></lay-select-option>
+          </lay-select>
+        </lay-form-item>
+        <lay-form-item label="手机号：">
+          <lay-input v-model="userInfo.phone"></lay-input>
+        </lay-form-item>
+        <lay-form-item label="邮箱地址：">
+          <lay-input v-model="userInfo.email"></lay-input>
+        </lay-form-item>
+        <lay-form-item label="个性签名："></lay-form-item>
+        <lay-textarea placeholder="请输入个性签名" v-model="userInfo.sign"></lay-textarea>
+        <br>
+        <lay-button type="primary" fluid @click="onUpdate()">保存个人信息</lay-button>
+      </lay-form>
+    </lay-panel>
 
 </template>
 
@@ -61,30 +61,30 @@ function onUpdate() {
 <style scoped lang="scss">
 .information {
   font-size: 18px;
-  height: 620px;
-  width: 400px;
+  height: 100%;
+  width: 100%;
   color: #ffffff;
   line-height: 38px;
-  background-color: #638863;
+  background-color: #87b087;
   border: 3px solid #3a5e3a;
   border-radius: 5px;
+  h1{
+    display: flex;
+    justify-content: center;
+  }
 }
-
+.layui-form{
+  margin-left: 100px;
+  margin-right: 100px;
+}
 .layui-icon {
   float: right;
 }
 
-h1 {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  border-bottom: 5px solid #3a5e3a;
-}
 
 .layui-avatar {
-  height: 100px;
-  width: 100px;
-  left: 35%;
+  height: 150px;
+  width: 150px;
   margin-bottom: 5px;
 }
 
@@ -99,5 +99,7 @@ h1 {
 .layui-textarea {
   background-color: #638863;
 }
+.layui-select{
 
+}
 </style>
